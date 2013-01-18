5412a715427db45741471545cd2a45a7c5627743
Steve Iboubi <steve.iboubibangole@us.sogeti.com>
2013-01-18 14:55:41 -0600

    Making the predictionArray an instance variable of the ViewController so it available outside the scope of the buttonPressed action

--- CrystalBall/ViewController.h
+++ CrystalBall/ViewController.h
@@ -8,7 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    NSArray *predictionArray;
+}
 - (IBAction)buttonPressed:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UILabel *predictionLabel;
 
